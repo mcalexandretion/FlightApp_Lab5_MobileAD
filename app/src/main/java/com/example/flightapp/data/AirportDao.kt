594db4bc8e7fb8dadb5ff52548564d1e550ff6a0
Alexandra <sashaf708@gmail.com>
2025-05-25 18:36:48 +0400

    feat: enable route sorting by descending passenger count

--- app/src/main/java/com/example/flightapp/data/AirportDao.kt
+++ app/src/main/java/com/example/flightapp/data/AirportDao.kt
@@ -6,12 +6,19 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AirportDao {
-    @Query("SELECT * FROM airport WHERE LOWER(iata_code) LIKE LOWER('%' || :query || '%') OR LOWER(name) LIKE LOWER('%' || :query || '%')")
+//    @Query("SELECT * FROM airport WHERE LOWER(iata_code) LIKE LOWER('%' || :query || '%') OR LOWER(name) LIKE LOWER('%' || :query || '%')")
+    @Query("""
+      SELECT * FROM airport
+      WHERE LOWER(iata_code) LIKE LOWER('%' || :query || '%')
+         OR LOWER(name) LIKE LOWER('%' || :query || '%')
+      ORDER BY passengers DESC
+    """)
     fun searchAirports(query: String): Flow<List<Airport>>
 
     @Query("SELECT * FROM airport WHERE iata_code != :from")
     fun getDestinations(from: String): Flow<List<Airport>>
 
-    @Query("SELECT * FROM airport")
+//    @Query("SELECT * FROM airport")
+    @Query("SELECT * FROM airport ORDER BY passengers DESC")
     fun getAllAirports(): Flow<List<Airport>>
 }
